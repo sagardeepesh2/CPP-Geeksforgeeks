48936b3094d70ce321d3000377c3405291c3c34e
Yuvraj Chandra <Singhyuvraj179@gmail.com>
2020-09-15 19:50:36 +0530

    Added dataTypes.cpp

---
+++ C++ Programs/dataTypes.cpp
@@ -0,0 +1,15 @@
+void dataTypes(int a, float b, double c, long long l,string d){
+  
+    float p =  b/c; //do b/c
+
+    double q =  b/a; //do b/a
+
+    int r =  c/a ; // do c/a
+
+    long long m = r+l;//do r+l
+
+    cout << p << " " << q << " " << r <<" "<< m <<endl;
+
+    cout 7<< d << " " << d[3] << endl;//write 3 for index
+
+}
