aaac23136ce54c727d4e36514df692df8fd073db
sandali-singh <72544086+sandali-singh@users.noreply.github.com>
2020-10-10 20:38:41 +0530

    Create string_to_char.cpp

---
+++ C++ Programs/string_to_char.cpp
@@ -0,0 +1,21 @@
+#include <bits/stdc++.h> 
+  using namespace std; 
+int main() 
+{ 
+    // assigning value to string s 
+    string s = "geeksforgeeks"; 
+  
+    int n = s.length(); 
+  
+    // declaring character array 
+    char char_array[n + 1]; 
+  
+    // copying the contents of the 
+    // string to char array 
+    strcpy(char_array, s.c_str()); 
+  
+    for (int i = 0; i < n; i++) 
+        cout << char_array[i]; 
+  
+    return 0; 
+} 
