3ebaa43cd019c11e65c62f9eb009a448d0d65d65
SHIVAM SINGH <72193756+Shivamsingh-c17@users.noreply.github.com>
2021-10-01 21:09:47 +0530

    Create Create_Swap_all_odd_and_even_bits.cpp

---
+++ Create_Swap_all_odd_and_even_bits.cpp
@@ -0,0 +1,44 @@
+//Initial Template for C++
+
+#include<bits/stdc++.h>
+using namespace std;
+
+
+ // } Driver Code Ends
+//User function Template for C++
+
+class Solution
+{
+    public:
+    //Function to swap odd and even bits.
+    unsigned int swapBits(unsigned int n)
+    {
+        unsigned int even_bits = n & 0xAAAAAAAA;
+
+        unsigned int odd_bits = n & 0x55555555;
+
+        even_bits >>= 1;
+        odd_bits <<= 1;
+
+        return (even_bits | odd_bits);
+    }
+};
+
+// { Driver Code Starts.
+
+// Driver code
+int main()
+{
+	int t;
+	cin>>t;//testcases
+	while(t--)
+	{
+		unsigned int n;
+		cin>>n;//input n
+
+		Solution ob;
+		//calling swapBits() method
+		cout << ob.swapBits(n) << endl;
+	}
+	return 0;
+}  // } Driver Code Ends
